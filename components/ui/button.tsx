4fbd665bb29e87aad7c7227dfe719fe8f2ef6187
Your Name <shahrizodazayniddinova7@gmail.com>
2025-02-20 16:30:48 +0500

    Auth page

---
+++ components/ui/button.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+
+interface ButtonProps {
+    label: ReactNode | string;
+    secondary?: boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    disabled?: boolean;
+    outline?: boolean;
+    type?: "button" | "submit";
+    onClick?: () => void;
+}
+
+export default function Button({
+    label,
+    secondary,
+    fullWidth,
+    large,
+    disabled,
+    outline,
+    type,
+    onClick
+} : ButtonProps) {
+  return (
+    <button 
+      disabled={disabled} onClick={onClick} type={type} 
+      className={`rounded-full font-semibold border transition hover:opacity-80 disabled:opacity-70 disabled:cursor-not-allowed 
+      ${fullWidth ? 'w-full' : 'w-fit'}
+      ${secondary ? 'bg-white text-black' : 'bg-sky-500 text-white'}
+      ${large ? 'text-xl px-5 py-3' : 'text-md px-4 py-3'}
+      ${outline ? 'bg-transparent border-slate-600 text-sky-500 hover:bg-slite-800/40' : ''}`}>
+        {label}
+    </button>
+  )
+}
